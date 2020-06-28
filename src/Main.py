72fd2bbb96e8dcf5709d9d831119284b962e1ea8
Amaar Quadri <amaarquadri@gmail.com>
2020-06-27 23:49:59 -0400

    moved main code to Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,21 +1,27 @@
 from src.games.Amazons import Amazons as Game
 from src.move_selection.MCTS import AsyncMCTS
+from src.ui.pygame_ui import PygameUI
+
+
+def main():
+    pygame_ui = PygameUI(Game)
+    move_chooser = AsyncMCTS(Game, pygame_ui.get_position(), time_limit=3, threads=1)
+    move_chooser.start()
+
+    while True:
+        user_chosen_position = pygame_ui.get_user_move()
+        if Game.is_over(user_chosen_position):
+            break
+
+        ai_chosen_position = move_chooser.choose_move(user_chosen_position)
+        pygame_ui.draw(ai_chosen_position)
+        if Game.is_over(ai_chosen_position):
+            break
+    print('Winner: ', Game.get_winner(pygame_ui.get_position()))
+
+    move_chooser.terminate()
+    pygame_ui.quit_on_x()
+
 
 if __name__ == '__main__':
-    board = Game()
-    move_chooser = AsyncMCTS(Game, board.get_state())
-    move_chooser.start()
-    time_limit = 3
-
-    while not Game.is_over(board.get_state()):
-        board.draw(move_prompt=True)
-        print('\n')
-
-        user_choice = input("Pick a Move: ")
-        board.perform_user_move(user_choice)
-
-        board.draw()
-        print("\n")
-        board.set_state(move_chooser.choose_move(board.get_state()))
-    move_chooser.terminate()
-    print("Result: ", Game.get_winner(board.get_state()))
+    main()
--- src/ui/pygame_ui.py
+++ src/ui/pygame_ui.py
@@ -1,5 +1,3 @@
-from src.games.TicTacToe import TicTacToe as Game
-from src.move_selection.MCTS import AsyncMCTS
 import pygame
 from src.utils.Utils import iter_product
 
@@ -83,27 +81,3 @@ class PygameUI:
                         return True
                     if event.button == pygame.BUTTON_RIGHT:
                         return False
-
-
-def main():
-    pygame_ui = PygameUI(Game)
-    move_chooser = AsyncMCTS(Game, pygame_ui.get_position(), time_limit=3, threads=1)
-    move_chooser.start()
-
-    while True:
-        user_chosen_position = pygame_ui.get_user_move()
-        if Game.is_over(user_chosen_position):
-            break
-
-        ai_chosen_position = move_chooser.choose_move(user_chosen_position)
-        pygame_ui.draw(ai_chosen_position)
-        if Game.is_over(ai_chosen_position):
-            break
-    print('Winner: ', Game.get_winner(pygame_ui.get_position()))
-
-    move_chooser.terminate()
-    pygame_ui.quit_on_x()
-
-
-if __name__ == '__main__':
-    main()
