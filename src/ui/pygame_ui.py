d2d1b0cad885312c7469cbb3a850a5d5ee608204
Amaar Quadri <amaarquadri@gmail.com>
2020-06-30 14:19:44 -0400

    supressed pygame startup message, added functions for resetting the board, and having the user enter a custom position

--- src/ui/pygame_ui.py
+++ src/ui/pygame_ui.py
@@ -1,5 +1,7 @@
-import pygame
 from src.utils.Utils import iter_product
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
+import pygame
 
 
 class PygameUI:
@@ -14,6 +16,9 @@ class PygameUI:
             starting_position = GameClass.STARTING_STATE
         self.board = GameClass(starting_position)
         self.draw()
+
+    def reset_board(self):
+        self.draw(self.GameClass.STARTING_STATE)
 
     def get_position(self):
         return self.board.get_state()
@@ -49,7 +54,34 @@ class PygameUI:
                             return self.board.get_state()
                         except ValueError:
                             print('Invalid Move! Try again.')
-                            return self.get_user_move()  # recurse
+                            clicks = []
+
+    def set_user_position(self):
+        self.flush()
+
+        clicks = []
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return None
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == pygame.BUTTON_LEFT:
+                        x, y = event.pos
+                        clicks.append((y // 64, x // 64))  # Note pygame inverst x and y
+                        if len(clicks) == self.GameClass.CLICKS_PER_MOVE:
+                            try:
+                                self.board.perform_user_move(clicks)
+                                self.draw()
+                            except ValueError:
+                                print('Invalid Move! Try again.')
+                            clicks = []
+                    elif event.button == pygame.BUTTON_RIGHT:
+                        return
+
+    @staticmethod
+    def flush():
+        pygame.event.clear(pygame.MOUSEBUTTONDOWN)
 
     @staticmethod
     def quit_on_x():
