b0a0dd2eb1a2b96cf69c9ef9d2adb2b30d77b3bd
Amaar Quadri <amaarquadri@gmail.com>
2020-07-05 18:04:25 -0400

    created worker that performs multiple games in series, batches the requests to the network and sends them all at once. This increases load on CPU per request to GPU so that the CPU doesn't run out of work while waiting for the GPU to finish (the other section)
    updated create_training_games for combined policy and evaluation model
    added function for dual architecture training with 2 sets of threads that interweave with GPU usage

--- src/heuristics/MCTSTraining.py
+++ src/heuristics/MCTSTraining.py
@@ -8,6 +8,70 @@ from src.move_selection.MCTS import RolloutNode, HeuristicNode
 from src.heuristics.Network import Network
 
 
+def simulate_game_batches_worker_process(GameClass, response_pipe, network, path='../heuristics/games/raw_mcts_games',
+                                         expansions_per_move=800, game_batch_count=3, c=np.sqrt(2), d=1):
+    """
+    Simulates several games in series, and aggregates and batches all their network call requests.
+    """
+    network.initialize()
+    training_data_sets = [[] for _ in range(game_batch_count)]
+    starting_policy, starting_evaluation = network.call(GameClass.STARTING_STATE[np.newaxis, ...])[0]
+    roots = [HeuristicNode(GameClass.STARTING_STATE, parent=None, GameClass=GameClass, network=None, c=c, d=d,
+                           network_call_results=(np.copy(starting_policy), starting_evaluation))
+             for _ in range(game_batch_count)]
+
+    while True:
+        best_nodes = []
+        for i in range(game_batch_count):
+            root = roots[i]
+            if root.count_expansions() >= expansions_per_move:
+                print('Making move')
+                best_node, distribution = root.choose_best_node(return_probability_distribution=True)
+                training_data_sets[i].append((root.position, distribution))
+                root = best_node
+                root.parent = None
+                if GameClass.is_over(root.position):
+                    print('Saving game')
+                    game = (training_data_sets[i], GameClass.get_winner(root.position))
+                    with open(f'{path}/game{time()}.pickle', 'wb') as fout:
+                        pickle.dump(game, fout)
+                    response_pipe.send(game)
+
+                    training_data_sets[i] = []
+                    root = HeuristicNode(GameClass.STARTING_STATE, parent=None, GameClass=GameClass, network=None,
+                                         c=c, d=d, network_call_results=(np.copy(starting_policy), starting_evaluation))
+
+                roots[i] = root
+
+            best_node = root.choose_expansion_node()
+            if best_node is None:
+                while root.children is not None:
+                    best_node, distribution = root.choose_best_node(return_probability_distribution=True)
+                    training_data_sets[i].append((root.position, distribution))
+                    root = best_node
+                    root.parent = None
+                game = (training_data_sets[i], GameClass.get_winner(root.position))
+                with open(f'{path}/game{time()}.pickle', 'wb') as fout:
+                    pickle.dump(game, fout)
+                response_pipe.send(game)
+                training_data_sets[i] = []
+                root = HeuristicNode(GameClass.STARTING_STATE, parent=None, GameClass=GameClass, network=None,
+                                     c=c, d=d, network_call_results=(np.copy(starting_policy), starting_evaluation))
+                roots[i] = root
+                best_node = root.choose_expansion_node()
+            best_nodes.append(best_node)
+
+        best_nodes_moves = [GameClass.get_possible_moves(best_node.position) for best_node in best_nodes]
+        network_call_results_batch = network.call(np.stack([position for moves in best_nodes_moves
+                                                            for position in moves], axis=0))
+        pos = 0
+        for best_node, moves in zip(best_nodes, best_nodes_moves):
+            new_pos = pos + len(moves)
+            network_call_results = network_call_results_batch[pos:new_pos]
+            best_node.expand(moves, network_call_results)
+            pos = new_pos
+
+
 def simulate_games_worker_process(GameClass, response_pipe, path='../heuristics/games/raw_mcts_games',
                                   expansions_per_move=800, network=None, c=np.sqrt(2), d=1):
     # format for each game file: ([(position, [pi_0, pi_1, ...]), ...], result)
@@ -43,20 +107,20 @@ def simulate_games_worker_process(GameClass, response_pipe, path='../heuristics/
         response_pipe.send(game)
 
 
-def create_training_games(GameClass, num_games=1000, threads=1, network_iteration=0,
-                          policy_path=None, evaluation_path=None, c=np.sqrt(2), d=1):
+def create_training_games(GameClass, num_games=10_000, threads=14, network_iteration=0,
+                          model_path=None, c=np.sqrt(2), d=1):
     parent_training_data_pipe, worker_training_data_pipe = Pipe(duplex=False)
-    if policy_path is None:
+    if model_path is None:
         path = '../heuristics/games/raw_mcts_games'
         worker_processes = [Process(target=simulate_games_worker_process,
-                                    args=(GameClass, worker_training_data_pipe, path, 500))
+                                    args=(GameClass, worker_training_data_pipe, path, 800))
                             for _ in range(threads)]
         network_process = None
     else:
         path = f'../heuristics/games/mcts_network{network_iteration}_games'
-        network_process, network_proxies = Network.spawn_process(GameClass, policy_path, evaluation_path, threads)
+        network_process, network_proxies = Network.spawn_process(GameClass, model_path, threads)
         worker_processes = [Process(target=simulate_games_worker_process,
-                                    args=(GameClass, worker_training_data_pipe, path, 100, network_proxy, c, d))
+                                    args=(GameClass, worker_training_data_pipe, path, 800, network_proxy, c, d))
                             for network_proxy in network_proxies]
         network_process.start()
 
@@ -75,17 +139,57 @@ def create_training_games(GameClass, num_games=1000, threads=1, network_iteratio
     return games
 
 
-def train_network(GameClass=Connect4, iterations=8, threads=14):
-    for iteration in range(iterations):
-        games = create_training_games(GameClass, threads=threads, network_iteration=iteration,
-                                      policy_path=f'../heuristics/models/policy{iteration}.h5',
-                                      evaluation_path=f'../heuristics/models/evaluation{iteration}.h5')
+def create_dual_architecture_training_games(GameClass, num_games=10_000, threads_per_section=14,
+                                            game_batch_count=4, network_iteration=0, c=np.sqrt(2), d=1):
+    path = f'../heuristics/games/mcts_network{network_iteration}_games'
+    model_path = f'../heuristics/models/model{network_iteration}.h5'
+    network_process, network_a_proxies, network_b_proxies = Network.spawn_dual_architecture_process(GameClass,
+                                                                                                    model_path,
+                                                                                                    threads_per_section)
+    parent_training_data_pipe, worker_training_data_pipe = Pipe(duplex=False)
+    worker_a_processes = [Process(target=simulate_game_batches_worker_process,
+                                  args=(GameClass, worker_training_data_pipe, network_proxy, path, 800,
+                                        game_batch_count, c, d))
+                          for network_proxy in network_a_proxies]
+    worker_b_processes = [Process(target=simulate_game_batches_worker_process,
+                                  args=(GameClass, worker_training_data_pipe, network_proxy, path, 800,
+                                        game_batch_count, c, d))
+                          for network_proxy in network_b_proxies]
 
-        network = Network(GameClass, f'../heuristics/models/policy{iteration}.h5',
-                          f'../heuristics/models/evaluation{iteration}.h5')
-        network.train(games)
-        network.save(f'../heuristics/models/policy{iteration + 1}.h5',
-                     f'../heuristics/models/evaluation{iteration + 1}.h5')
+    # start all processes in the order that they are required
+    for worker_process in worker_a_processes:
+        worker_process.start()
+    network_process.start()
+    for worker_process in worker_b_processes:
+        worker_process.start()
+
+    games = [parent_training_data_pipe.recv() for _ in range(num_games)]
+
+    # terminate and join all processes
+    for worker_process in worker_a_processes + worker_b_processes:
+        worker_process.terminate()
+    for worker_process in worker_a_processes + worker_b_processes:
+        worker_process.join()
+    network_process.terminate()
+    network_process.join()
+
+    return games
+
+
+def train_network(GameClass=Connect4, iterations=8, threads=14):
+    import os
+    for iteration in range(iterations):
+        create_training_games(GameClass, threads=threads, network_iteration=iteration,
+                              model_path=f'../heuristics/models/model{iteration}.h5')
+
+        data = []
+        for file in os.listdir('../heuristics/games/raw_mcts_games'):
+            with open(f'../heuristics/games/raw_mcts_games/{file}', 'rb') as fin:
+                data.append(pickle.load(fin))
+
+        network = Network(GameClass, f'../heuristics/models/model{iteration}.h5')
+        network.train(data)
+        network.save(f'../heuristics/models/model{iteration + 1}.h5')
 
 
 def view_game(path):
@@ -109,5 +213,7 @@ def view_game(path):
 
 
 if __name__ == '__main__':
-    train_network(Connect4, 8, threads=14)
+    create_dual_architecture_training_games(Connect4, network_iteration=1,
+                                            threads_per_section=14, game_batch_count=50, num_games=10_000)
+    # train_network(Connect4, 1, threads=14)
     # view_game('../heuristics/games/raw_mcts_game/game.pickle')
