5be2faa366c9ebaa6d52e1ac466fba2c5bd24600
Amaar Quadri <amaarquadri@gmail.com>
2020-07-06 15:44:42 -0400

    enabled manually activating the venv in start_tensorboard.py

--- src/scripts/start_tensorboard.py
+++ src/scripts/start_tensorboard.py
@@ -1,8 +1,14 @@
 import os
 
 
-def start_tensor_board():
-    os.system('cmd /k "tensorboard --logdir ../heuristics/logs"')
+def start_tensor_board(enable_venv_manually=True):
+    venv_command = 'cd ../../venv/Scripts && activate && cd ../../src/scripts'
+    tensor_board_command = 'tensorboard --logdir ../heuristics/logs'
+    if enable_venv_manually:
+        command = f'cmd /k "{venv_command} && {tensor_board_command}"'
+    else:
+        command = f'cmd /k "{tensor_board_command}"'
+    os.system(command)
 
 
 if __name__ == '__main__':
