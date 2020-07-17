985b258295cb8d79df50b8fea72dfca0ae816185
Amaar Quadri <amaarquadri@gmail.com>
2020-07-17 15:56:45 -0400

    disabled manual venv activation for tensorboard

--- src/scripts/start_tensorboard.py
+++ src/scripts/start_tensorboard.py
@@ -2,10 +2,10 @@ import os
 from src.games.Connect4 import Connect4
 
 
-def start_tensor_board(GameClass, enable_venv_manually=True):
-    venv_command = 'cd ../../venv/Scripts && activate && cd ../../src/scripts'
+def start_tensor_board(GameClass, enable_venv_manually=False):
     tensor_board_command = f'tensorboard --logdir ../heuristics/{GameClass.__name__}/logs'
     if enable_venv_manually:
+        venv_command = 'cd ../../venv/Scripts && activate && cd ../../src/scripts'
         command = f'cmd /k "{venv_command} && {tensor_board_command}"'
     else:
         command = f'cmd /k "{tensor_board_command}"'
