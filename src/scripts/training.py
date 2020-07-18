d68147a1303700e528370a0931d2bd2f2bd1041d
Amaar Quadri <amaarquadri@gmail.com>
2020-07-18 16:32:03 -0400

    moved reinforcement learning pipeline to separate file with classes also moved rollout game generation to same file used queue instead of pipes to remove deadlock added replay buffer in separate process used event to gently stop rollout games instead of immediately terminating used easygui to show prompt to stop training on main thread

---
+++ src/scripts/training.py
@@ -0,0 +1,40 @@
+import numpy as np
+import easygui
+from src.games.Connect4 import Connect4 as GameClass
+from src.learning.learning import SelfPlayReinforcementLearning, MCTSRolloutGameGenerator
+
+
+def main():
+    trainer = SelfPlayReinforcementLearning(GameClass,
+                                            f'../heuristics/{GameClass.__name__}/models/model-reinforcement.h5',
+                                            threads_per_section=14, game_batch_count=7, expansions_per_move=1500,
+                                            c=np.sqrt(2), d=1)
+    # trainer = MCTSRolloutGameGenerator(GameClass, threads=14, c=np.sqrt(2))
+    trainer.start()
+    easygui.msgbox('Click to end training', title=f'{GameClass.__name__} Training', ok_button='End Training')
+    print('Ending training')
+    trainer.terminate()
+
+
+def sanitize():
+    import os
+    import pickle
+    from shutil import copyfile
+    for file in os.listdir(f'../heuristics/{GameClass.__name__}/games/mcts_network0_games'):
+        error = False
+        try:
+            with open(f'../heuristics/{GameClass.__name__}/games/mcts_network0_games' + '/' + file, 'rb') as fin:
+                pickle.load(fin)
+        except:
+            error = True
+        if error:
+            pass
+            # copyfile(f'../heuristics/{GameClass.__name__}/games/raw_mcts_games/' + file,
+            #          f'../heuristics/{GameClass.__name__}/games/raw_mcts_games/dead/' + file)
+        else:
+            copyfile(f'../heuristics/{GameClass.__name__}/games/mcts_network0_games/' + file,
+                     f'../heuristics/{GameClass.__name__}/games/mcts_network0_games/temp/' + file)
+
+
+if __name__ == '__main__':
+    main()
