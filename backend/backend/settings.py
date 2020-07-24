39d34611ab7d3fa084255b4826ac775725598b9f
Amaar Quadri <amaarquadri@gmail.com>
2020-07-24 15:48:28 -0400

    reverted to whitelist only localhost:3000 this is working fine on my desktop, but not on my laptop???

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,8 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'connect4', 'static', 'connect4'),)
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
-# CORS_ORIGIN_WHITELIST = [
-#     "http://localhost:3000"
-# ]
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
+]
