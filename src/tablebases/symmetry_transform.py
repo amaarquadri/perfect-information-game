7697ed9a68e45bedee1da1f79997dd9f2990b57f
Amaar Quadri <amaarquadri@gmail.com>
2021-02-21 04:53:41 -0500

    add color inversion and inverse transformations to symmetry_transform.py

--- src/tablebases/symmetry_transform.py
+++ src/tablebases/symmetry_transform.py
@@ -7,39 +7,52 @@ class SymmetryTransform:
     # noinspection PyChainedComparisons
     UNIQUE_SQUARE_INDICES = [(i, j) for i, j in iter_product(Chess.BOARD_SHAPE) if i < 4 and j < 4 and i <= j]
 
-    @staticmethod
-    def requires_transform(i, j):
-        # noinspection PyChainedComparisons
-        return not (i < 4 and j < 4 and i <= j)
+    def __init__(self, state):
+        self.transform_funcs = []
 
-    def __init__(self, state, attacking_slice):
-        i, j = Chess.get_king_pos(state, attacking_slice)
-        if i < 4:
-            self.flip_i = False
+        if Chess.heuristic(state) < 0:
+            # black is attacking, so switch white and black
+            self.transform_funcs.append(SymmetryTransform.flip_state_colors)
+            i, j = Chess.get_king_pos(state, Chess.BLACK_SLICE)
         else:
-            self.flip_i = True
-            i = 7 - i
-        if j < 4:
-            self.flip_j = False
-        else:
-            self.flip_j = True
-            j = 7 - j
-        self.flip_diagonal = not (i <= j)
+            i, j = Chess.get_king_pos(state, Chess.WHITE_SLICE)
 
-    def transform_coordinates(self, i, j):
-        if self.flip_i:
-            i = 7 - i
-        if self.flip_j:
-            j = 7 - j
-        if self.flip_diagonal:
-            i, j = j, i
-        return i, j
+        if not (i < 4):
+            self.transform_funcs.append(SymmetryTransform.flip_state_i)
+            i = Chess.ROWS - 1 - i
+        if not (j < 4):
+            self.transform_funcs.append(SymmetryTransform.flip_state_j)
+            j = Chess.COLUMNS - 1 - j
+        if not (i <= j):
+            self.transform_funcs.append(SymmetryTransform.flip_state_diagonal)
+
+    def is_identity(self):
+        return len(self.transform_funcs) == 0
 
     def transform_state(self, state):
-        if self.flip_i:
-            state = np.flip(state, axis=0)
-        if self.flip_j:
-            state = np.flip(state, axis=1)
-        if self.flip_diagonal:
-            state = np.rot90(np.flip(state, axis=1), axes=(0, 1))
+        for transform_func in self.transform_funcs:
+            state = transform_func(state)
         return state
+
+    def untransform_state(self, state):
+        for transform_func in self.transform_funcs[::-1]:
+            state = transform_func(state)
+        return state
+
+    @staticmethod
+    def flip_state_colors(state):
+        special_layers = state[..., -2:]
+        special_layers[..., -1] = 1 - special_layers[..., -1]
+        return np.concatenate((state[..., Chess.BLACK_SLICE], state[..., Chess.WHITE_SLICE], special_layers), axis=-1)
+
+    @staticmethod
+    def flip_state_i(state):
+        return np.flip(state, axis=0)
+
+    @staticmethod
+    def flip_state_j(state):
+        return np.flip(state, axis=1)
+
+    @staticmethod
+    def flip_state_diagonal(state):
+        return np.rot90(np.flip(state, axis=1), axes=(0, 1))
