f31cac2a311a94f9c98eda268872da90950c6d01
Amaar Quadri <amaarquadri@gmail.com>
2021-03-03 20:53:33 -0500

    added empty iterative_deepening.py

---
+++ src/move_selection/iterative_deepening.py
@@ -0,0 +1,9 @@
+from move_selection.move_chooser import MoveChooser
+
+
+class IterativeDeepening(MoveChooser):
+    def __init__(self):
+        pass
+
+    def choose_move(self, return_distribution=False):
+        pass
