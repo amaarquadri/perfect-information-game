327fb388cc59ed1c41e182537ff7d76e807544ef
Amaar Quadri <amaarquadri@gmail.com>
2021-03-03 20:53:18 -0500

    improved test for minimax

--- tests/move_selection/test_mini_max.py
+++ tests/move_selection/test_mini_max.py
@@ -6,7 +6,10 @@ from games.tic_tac_toe import TicTacToe as GameClass
 class TestMiniMax(unittest.TestCase):
     def test_solver(self):
         mini_max = MiniMax.solver(GameClass)
-        print(mini_max.generate_random_game())
+        training_data, outcome = mini_max.generate_random_game()
+        if outcome != 0:
+            raise AssertionError('Tic tac toe solver did not draw the game!')
+        print(training_data, outcome)
 
 
 if __name__ == '__main__':
