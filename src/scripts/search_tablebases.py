a3caa39b4950e282df0629f72673aa560906a84e
Amaar Quadri <amaarquadri@gmail.com>
2021-03-07 06:02:32 -0500

    add puzzle blacklist

--- src/scripts/search_tablebases.py
+++ src/scripts/search_tablebases.py
@@ -20,20 +20,22 @@ def search_for_draws():
             print('-1: ', sum([manager.parse_move_bytes(data)[0] == -1 for data in nodes.values()]))
 
 
-def search_for_puzzles():
+def search_for_puzzles(descriptor='KBNk'):
     manager = TablebaseManager(GameClass)
-    for descriptor in ['KBNk']:
-        with open(f'{get_training_path(GameClass)}/tablebases/{descriptor}.pickle', 'rb') as file:
-            nodes = pickle.load(file)
-            print(descriptor)
-            longest_terminal_distance = -1
-            best_board_bytes = None
-            for board_bytes, move_bytes in nodes.items():
-                outcome, *_, terminal_distance = manager.parse_move_bytes(move_bytes)
-                if outcome == 1 and terminal_distance > longest_terminal_distance:
-                    best_board_bytes = board_bytes
-                    longest_terminal_distance = terminal_distance
-            print(GameClass.encode_fen(GameClass.parse_board_bytes(best_board_bytes)))
+    blacklist = ['K1k5/2N4B/8/8/8/8/8/8 w - - - -']
+
+    with open(f'{get_training_path(GameClass)}/tablebases/{descriptor}.pickle', 'rb') as file:
+        nodes = pickle.load(file)
+        print(descriptor)
+        longest_terminal_distance = -1
+        best_board_bytes = None
+        for board_bytes, move_bytes in nodes.items():
+            outcome, *_, terminal_distance = manager.parse_move_bytes(move_bytes)
+            if outcome == 1 and terminal_distance > longest_terminal_distance \
+                    and GameClass.encode_fen(GameClass.parse_board_bytes(board_bytes)) not in blacklist:
+                best_board_bytes = board_bytes
+                longest_terminal_distance = terminal_distance
+        print(GameClass.encode_fen(GameClass.parse_board_bytes(best_board_bytes)))
 
 
 def search_for_draw_by_repetition():
