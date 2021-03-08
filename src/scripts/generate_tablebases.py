710d2e06784a7fc9a02cb38922364dafa4da7877
Amaar Quadri <amaarquadri@gmail.com>
2021-03-07 21:08:32 -0500

    added descriptor groups

--- src/scripts/generate_tablebases.py
+++ src/scripts/generate_tablebases.py
@@ -6,11 +6,13 @@ from utils.active_game import ActiveGame as GameClass
 def generate_tablebases(threads=12):
     generator = TablebaseGenerator(GameClass)
 
-    THREE_MAN = 'KQk,KRk,KPk'  # KBk and KNk are theoretical draws
+    TWO_MAN = 'Kk'
+    THREE_MAN = 'KQk,KRk,KBk,KNk,KPk'
+    FOUR_MAN_NO_ENEMY_NO_DUPLICATE = 'KQRk,KQBk,KQNk,KRBk,KRNk,KBNk'
     FOUR_MAN_NO_ENEMY = 'KQQk,KQRk,KQBk,KQNk,KQPk,KRRk,KRBk,KRNk,KRPk,KBBk,KBNk,KBPk,KNNk,KNPk,KPPk'
     FOUR_MAN_WITH_ENEMY = 'KQkq,KQkr,KQkb,KQkn,KQkp,KRkr,KRkb,KRkn,KRkp,KBkb,KBkn,KBkp,KNkn,KNkp,KPkp'
     with OptionalPool(threads) as pool:
-        for section in [THREE_MAN, FOUR_MAN_WITH_ENEMY, FOUR_MAN_NO_ENEMY]:
+        for section in [FOUR_MAN_WITH_ENEMY]:
             for descriptor in section.split(','):
                 generator.generate_tablebase(descriptor, pool)
                 print(f'Completed {descriptor}')
