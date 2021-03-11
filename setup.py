cc391de6f360fc9433761c6d7e24c3877083c7eb
Amaar Quadri <amaarquadri@gmail.com>
2021-03-10 20:41:56 -0500

    move LICENSE.txt and setup.py to root folder

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.0.1'
+packages = find_packages()
 with open('README.md', 'r') as file:
     long_description = file.read()
 
 setup(name='perfect-information-game',
-      version='0.0.1',
+      version=VERSION,
       description='Create 2D perfect information board games, and play them with machine learning systems.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       license='MIT',
       py_modules=[],
       python_requires='>=3.8',
-      install_requires=['numpy', ],
-      extras_require={'dev': ['chess']},
+      install_requires=[
+          'numpy',
+          'tensorflow',
+          'tensorflowjs',
+          'keras',
+          'pygame',
+          'easygui'
+      ],
+      extras_require={'dev': [
+          'chess',
+          'memory_profiler',
+          'matplotlib',
+          'check-manifest',
+          'twine'
+      ]},
       package_dir={'': 'src'},
-      packages=find_packages(),
+      packages=packages,
       classifiers=['License :: OSI Approved :: MIT License',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.8',
@@ -23,3 +38,4 @@ setup(name='perfect-information-game',
       author='Amaar Quadri',
       author_email='amaarquadri@gmail.com')
 # TODO: see if there are more relevant classifiers: https://pypi.org/classifiers/
+# python setup.py bdist_wheel sdist
