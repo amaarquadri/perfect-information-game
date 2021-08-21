0226605914d679f38977308f1ef3a9ed06ed3319
Amaar Quadri <amaarquadri@gmail.com>
2021-08-21 17:52:21 -0400

    add option to allow pseudo legal moves in apply_from_to_move

--- perfect_information_game/games/chess.py
+++ perfect_information_game/games/chess.py
@@ -272,10 +272,15 @@ class Chess(Game):
         self.state = self.apply_from_to_move(self.state, start_i, start_j, end_i, end_j, promotion)
 
     @classmethod
-    def apply_from_to_move(cls, state, start_i, start_j, end_i, end_j, promotion=None):
+    def apply_from_to_move(cls, state, start_i, start_j, end_i, end_j, promotion=None, allow_pseudo_legal=False):
         friendly_slice, enemy_slice, *_ = cls.get_stats(state)
 
-        for move in cls.get_possible_moves(state):
+        if allow_pseudo_legal:
+            _, _, allowable_moves, _ = cls.get_pseudo_legal_moves(state)
+        else:
+            allowable_moves = cls.get_possible_moves(state)
+
+        for move in allowable_moves:
             if np.any(state[start_i, start_j, friendly_slice] == 1) and \
                     np.all(state[end_i, end_j, friendly_slice] == 0) and \
                     np.all(move[start_i, start_j, :12] == 0) and \
