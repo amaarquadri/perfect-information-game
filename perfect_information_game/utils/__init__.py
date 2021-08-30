2ff7793c84cf938291154b236127383e25606a73
Amaar Quadri <amaarquadri@gmail.com>
2021-08-30 01:12:40 -0400

    add utility functions to utils.py

--- perfect_information_game/utils/__init__.py
+++ perfect_information_game/utils/__init__.py
@@ -1,2 +1,2 @@
 from perfect_information_game.utils.utils import OptionalPool, STRAIGHT_DIRECTIONS, DIAGONAL_DIRECTIONS, DIRECTIONS_8, \
-    get_training_path, choose_random, one_hot, iter_product
+    get_training_path, choose_random, one_hot, iter_product, get_np_uint_type, alternate_iterables
